ee7405512cb6546d09f921f50bee484b80ecc3ca
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-03-29 17:33:47 +1300

    Initial Build of program
    This is the initial build of the 02_Mystery_Box program, with a basic outline being made at this point in the process.

---
+++ 01_Mystery_Box_Outline.py
@@ -0,0 +1,17 @@
+# Initial outline from Support Files.
+
+from tkinter import *
+import random
+
+
+class Foo:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Foo(root)
+    root.mainloop()
