84b6e1ec44ec3e6985230cbafbf0c431e405c8bf
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-04-08 11:47:37 +1200

    Functional Start GUI build with visual improvements
    This is the most recent functional build of the Start GUI.

--- 02_Start_GUI.py
+++ 02_Start_GUI.py
@@ -6,25 +6,29 @@ from tkinter import *
 class Start:
     def __init__(self, parent):
 
+        # From "02_Converter_GUI.py"
+        background = "#D5CFE1" # Grey
+
         # GUI to find starting balance and stakes from the user:
-        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame = Frame(padx=10, pady=10, bg=background)
         self.start_frame.grid()
 
         # Mystery Heading (Row 0)
         self.mystery_box_label = Label(self.start_frame, text="Mystery Box",
-                                       font="Arial 19 bold")
+                                       font="Arial 19 bold",
+                                       bg=background)
         self.mystery_box_label.grid(row=0)
 
         # Mystery Subheading (Row 1)
         self.mystery_subheading = Label(self.start_frame, text="Welcome to the Mystery Box Game",
-                                       font="Arial 10")
+                                       font="Arial 10", bg=background)
         self.mystery_subheading.grid(row=1)
 
         # Text wrap and Justify code from "01_Help_GUI.py".
         # First Instruction (Row 2)
         self.first_instruction = Label(self.start_frame, text="Please enter a dollar amount in the box below (between 5 and 50).",
                                        font="Arial 10 italic", wrap=300,
-                                       justify=LEFT)
+                                       justify=LEFT, bg=background)
         self.first_instruction.grid(row=2)
 
         # Entry Box (Row 3)
@@ -35,7 +39,7 @@ class Start:
         # Second Instruction (Row 4)
         self.second_instruction = Label(self.start_frame, text="After you have entered this number, please choose your stakes. The higher stakes that you choose, the higher your possible winnings are.",
                                        font="Arial 10 italic", wrap=300,
-                                       justify=LEFT)
+                                       justify=LEFT, bg=background)
         self.second_instruction.grid(row=4)
         
         # Inspired by "Conversion buttons frame" from "12g_Assembled_Program.py" as a part of "01_Temperature_Converter".
@@ -45,23 +49,28 @@ class Start:
 
         # Low Stakes Button (Row 0, Column 0)
         self.low_stakes_button = Button(self.stakes_buttons_frame,
+                                        command=lambda: self.to_game(1),
                                         text="Low: $5.00",
                                         bg="#04E762") # Green
         self.low_stakes_button.grid(row=0, column=0, padx=10)
 
         # Medium Stakes Button (Row 0, Column 1)
         self.medium_stakes_button = Button(self.stakes_buttons_frame,
-                                        text="Medium: $10.00")
+                                        command=lambda: self.to_game(2),
+                                        text="Medium: $10.00",
+                                        bg="#F5B700") # Orange
         self.medium_stakes_button.grid(row=0, column=1, padx=10)
 
         # Low Stakes Button (Row 0, Column 2)
         self.high_stakes_button = Button(self.stakes_buttons_frame,
-                                        text="High: $15.00")
+                                        command=lambda: self.to_game(3),
+                                        text="High: $15.00",
+                                        bg="#00A1E4") # Blue
         self.high_stakes_button.grid(row=0, column=2, padx=10)
 
         # Help Button (Row 6)
         self.help_button = Button(text="How to Play",
-                                        command=lambda: self.to_game(1))
+                                    command=lambda: self.to_game(4))
         self.help_button.grid(row=6, pady=10)
 
     def to_game(self, stakes):
