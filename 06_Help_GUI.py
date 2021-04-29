6d7cab531ee30177ddfa02b85aead2f99b11c040
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-04-29 13:21:26 +1200

    Build at beginning of Statistics GUI Video

--- 06_Help_GUI.py
+++ 06_Help_GUI.py
@@ -67,12 +67,17 @@ if __name__ == '__main__':
             self.how_heading.grid(row=0)
 
             # Help text definition
-            help_text = "Choose an amount to play with and"
+            help_text = "Choose an amount to play with and then choose the stakes. " \
+                        "Higher stakes cost more per round, but you can win more as well." \
+                        "When you enter the play area, you will see three 'mystery boxes'. "
+                        "To reveal the contents of these boxes, click the 'Open Boxes' button "
+                        "toward the bottom of the window. If you don't have enough money to continue playing, "
+                        "the button will turn red,"
 
             # Help text (Label, Row 1)
             self.help_text = Label(self.help_frame, text=help_text,
-                                   justify=LEFT, width=40,
-                                   bg=background, wrap=250)
+                                justify=LEFT, width=40,
+                                bg=background, wrap=250)
             self.help_text.grid(row=1)
 
             # Dismiss button (Row 2)
