a0f9b849795399714f32d69e21922622aca1f7f3
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-04-29 23:34:26 +1200

    Build with complete and disconnected code

--- 08_Game_Export_GUI.py
+++ 08_Game_Export_GUI.py
@@ -46,6 +46,7 @@ class Game:
     def to_statistics(self, game_history, game_statistics):
         GameStatistics(self, game_history, game_statistics)
 
+# Beginning of Game Statistics class (from "07b_Game_Statistics_GUI_Version_2.py")
 class GameStatistics:
 
     # Set up initial function
@@ -143,15 +144,182 @@ class GameStatistics:
         self.games_played_value_label = Label(self.details_frame, font=content,
                                                     text=len(game_history),
                                                     anchor="w")
-        self.win_or_loss_value_label.grid(row=3, column=1, padx=0)
+        self.games_played_value_label.grid(row=3, column=1, padx=0)
 
-        # Dismiss button [From "00_Compiled_Version_2.py"] (Row 4)
-        self.dismiss_button = Button(self.help_frame, text="Dismiss",
+        # Dismiss and Export buttons [From "05b_Game_Playable_With_Photos.py"] (Row 3)
+        self.dismiss_export_frame = Frame(self.statistics_frame)
+        self.dismiss_export_frame.grid(row=3, padx=10, pady=10)
+
+        # Dismiss button [From "00_Compiled_Version_2.py"] (Row 0, Column 0)
+        self.dismiss_button = Button(self.dismiss_export_frame, text="Dismiss",
                                         width =10, bg="pink", font="arial 10 bold",
-                                        command=partial(self.close_help, partner))
-        self.dismiss_button.grid(row=4, pady=10)
+                                        command=partial(self.close_statistics, partner))
+        self.dismiss_button.grid(row=0, column=0)
+
+        # Export button (Row 0, Column 1)
+        self.export_button = Button(self.dismiss_export_frame, text="Export",
+                                    font="Arial 15 bold", bg="#464655", fg="#D5CFE1")
+                                    # Lilac Text, Dark Grey Button (From "02_Start_GUI.py")
+        self.export_button.grid(row=0, column=1)
+    
+    # Closes statistics
+    def close_statistics(self, partner):
+
+        # Put statistics button back to normal:
+        partner.statistics_button.config(state=NORMAL)
+        self.statistics_box.destroy()
+
+class Export:
+
+    # Set up initial function
+    def __init__(self, partner, game_history, all_game_statistics):
+
+        print(game_history)
+
+        # Disable history button
+        partner.export_button.config(state=DISABLED)
+
+        heading = "Arial 12 bold"
+        content = "Arial 12"
+
+        # Sets up child window (or statistics box)
+        self.export_box = Toplevel()
+
+        # If users press cross at the top of the window, export closes and the export button 'releases'
+        self.export_box.protocol('WM_DELETE_WINDOW', partial(self.close_statistics, partner))
+
+        # Set up GUI Frame
+        self.export_frame = Frame(self.export_box, width=300)
+        self.export_frame.grid()
+
+        # Set up Export heading (Row 0)
+        self.export_heading = Label(self.export_frame,
+                                    text="Export and Instructions",
+                                    font="arial 19 bold")
+        self.export_heading.grid(row=0)
 
 
+
+        # Export Instructions (Row 1)
+        self.export_instructions = Label(self.statistics_frame,
+                                    text="Enter a file name in the box below "
+                                    "and press the Save button to save your"
+                                    "calculation history to a text file",
+                                    justify=LEFT,
+                                    font="arial 10", wrap=250,
+                                    padx=10, pady=10)
+        self.export_instructions.grid(row=1)
+
+        # Export Warning (Row 2)
+        self.export_warning = Label(self.statistics_frame,
+                                    text="if the file name that you enter "
+                                    "already exists, its contents will be "
+                                    "replaced with your calculation history",
+                                    justify=LEFT, bg="yellow", fg="orange",
+                                    font="arial 10", wrap=220,
+                                    padx=10, pady=10)
+        self.export_warning.grid(row=2)
+
+        # File Name Entry Box (Row 3) [From "12c_Export_GUI.py"]
+        self.file_name_entry = Entry(self.export_frame, width=20,
+                                font="Arial 14 bold", justify=CENTER)
+        self.file_name_entry.grid(row=3, pady=10)
+
+        # Error message labels (Row 4) [This is initially blank]
+        self.save_error_label = Label(self.export_frame, text="", fg="red")
+        self.save_error_label.grid(row=5, pady=10)
+
+        # Save/Cancel Button(s) Frame (Row 5) [From "12c_Export_GUI.py"]
+        self.save_cancel_frame = Frame(self.export_frame)
+        self.save_cancel_frame.grid(row=5, pady=10)
+
+        # Save and Cancel Buttons (Row 0 of the "save_cancel_frame").
+        # ... This portion of the code has been inspired by the file
+        # ... titled "02c_Converter_GUI_Version_3.py".
+
+        # Save Button
+        self.save_button = Button(self.save_cancel_frame,
+                                    text="Save", font="Arial 10 bold",
+                                    bg="pink", width=10,
+                                    command=partial (lambda: self.save_history(partner, game_history, all_game_statistics)))
+        self.save_button.grid(row=0, column=0)
+
+        self.dismiss_button = Button(self.save_cancel_frame,
+                                    font="Arial 10 bold",
+                                    text="Dismiss",
+                                    command=partial(self.close_export, partner),
+                                    bg="yellow", width=10)
+        self.dismiss_button.grid(row=0, column=1)
+
+    # Defining "save_history" function (from "12d_History_and_Export_GUI.py")
+    def save_history(self, partner, game_history, game_statistics):
+
+        # Uses a regular expression to check of a file name is valid
+        valid_character = "[A-Za-z0-9_]"
+        has_error = "no"
+
+        file_name = self.file_name_entry.get()
+        print(file_name)
+
+        for letter in file_name:
+            if re.match(valid_character, letter):
+                continue
+
+            elif letter == " ":
+                problem = "(Spaces are not allowed in this context)"
+
+            else:
+                problem = ("(The character of {} is not alowed in this context)".format(letter))
+
+            has_error = "yes"
+            break
+
+        if file_name == "":
+            problem = "The file name for this cannot be blank"
+            has_error = "yes"
+
+        if has_error == "yes":
+
+            # Display Error Message
+            self.save_error_label.config(text="Invalid file name - {}".format(problem))
+
+            # Change entry box background to pink
+            self.file_name_entry.config(bg="pink")
+            print()
+
+        else:
+            # If there are no errors, generate text file and then close dialogue
+            # Add .txt suffix to file name
+            file_name = file_name + ".txt"
+
+            # Create file to hold data
+            f = open(file_name, "w+")
+
+            # Heading for Statistics
+            f.write("Game Statistics\n\n")
+
+            # Game Statistics
+            for round in game_statistics:
+                f.write(round + "\n")
+
+            # Heading for Rounds
+            f.write("\nRound Details\n\n")
+
+            # Add new line at end of each item
+            for item in game_history:
+                f.write(item + "\n")
+
+            # Close file
+            f.close()
+
+            # Close dialogue
+            self.close_export(partner)
+
+    # Defining close_export function
+    def close_export(self, partner):
+        # Put export button back into a normal state..
+        partner.export_button.config(state=NORMAL)
+        self.export_box.destroy()
 
 # From "02_Start_GUI.py"
 # main routine
